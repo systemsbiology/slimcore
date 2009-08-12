66f9a616a76200c8afaa6aaaab080914a9fe90fd
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-08-12 09:24:43 -0700

    Upgrade to Rspec 1.2.7

--- spec/controllers/lab_groups_routing_spec.rb
+++ spec/controllers/lab_groups_routing_spec.rb
@@ -11,19 +11,21 @@ describe LabGroupsController do
     end
   
     it "should map #show" do
-      route_for(:controller => "lab_groups", :action => "show", :id => 1).should == "/lab_groups/1"
+      route_for(:controller => "lab_groups", :action => "show", :id => "1").should == "/lab_groups/1"
     end
   
     it "should map #edit" do
-      route_for(:controller => "lab_groups", :action => "edit", :id => 1).should == "/lab_groups/1/edit"
+      route_for(:controller => "lab_groups", :action => "edit", :id => "1").should == "/lab_groups/1/edit"
     end
   
     it "should map #update" do
-      route_for(:controller => "lab_groups", :action => "update", :id => 1).should == "/lab_groups/1"
+      route_for(:controller => "lab_groups", :action => "update", :id => "1").should == 
+        {:path => "/lab_groups/1", :method => :put}
     end
   
     it "should map #destroy" do
-      route_for(:controller => "lab_groups", :action => "destroy", :id => 1).should == "/lab_groups/1"
+      route_for(:controller => "lab_groups", :action => "destroy", :id => "1").should ==
+        {:path => "/lab_groups/1", :method => :delete}
     end
   end
 
