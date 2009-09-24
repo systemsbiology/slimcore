f532ad19116242057eb7fca2839aca43c5582279
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-09-24 11:33:40 -0700

    Sory lab groups by name by default

--- app/models/lab_group.rb
+++ app/models/lab_group.rb
@@ -2,5 +2,7 @@ class LabGroup < ActiveRecord::Base
   has_many :lab_memberships, :dependent => :destroy
   has_many :users, :through => :lab_memberships
 
+  default_scope :order => "name ASC"
+
   validates_uniqueness_of :name
 end
